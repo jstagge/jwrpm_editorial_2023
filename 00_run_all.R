c0d86638d8f4d70440191e6cc232a476427f984c
jstagge <jhstagge@gmail.com>
2023-11-14 09:43:35 -0500

    Initial commit

---
+++ 00_run_all.R
@@ -0,0 +1,16 @@
+# *------------------------------------------------------------------
+# | FILE NAME: 00_run_all.R
+# | DATE: 11/14/2023
+# | CREATED BY:  Jim Stagge
+# *----------------------------------------------------------------
+# | PURPOSE:  Runs all code in the proper order to generate Fig 1 of
+# |           "JWRP&M’s Reproducibility Review Program: Accomplishments, Lessons, and Next Steps"
+# |
+# *------------------------------------------------------------------
+
+###########################################################################
+## Set the Paths
+###########################################################################
+
+source("01_preparation.R")
+source("02_jwrpm_fig1.R")
